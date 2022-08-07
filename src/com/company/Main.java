a4e8c1b7b9c45d46052d0661f486a1ab3a68d066
VasilievNik <nikita.veselyev@gmail.com>
2022-08-07 20:50:07 +0300

    3d commit. Faster sorting and finding added

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,18 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(StringList.add("a"));
-        System.out.println(StringList.add(2,"b"));
-        System.out.println(StringList.set(1,"c"));
-        System.out.println(StringList.remove("c"));
-        StringList.remove(2);
-        System.out.println(StringList.contains("b"));
-        System.out.println(StringList.indexOf("b"));
-        System.out.println(StringList.lastIndexOf("b"));
-        System.out.println(StringList.get(2));
-        System.out.println(StringList.size());
-        System.out.println(StringList.isEmpty());
-        System.out.println(StringList.toArray());
-        StringList.clear();
+        System.out.println(IntegerList.add(1));
+        System.out.println(IntegerList.add(2,2));
+        System.out.println(IntegerList.set(1,3));
+        IntegerList.remove(2);
+        System.out.println(IntegerList.contains(2));
+        System.out.println(IntegerList.indexOf(2));
+        System.out.println(IntegerList.lastIndexOf(3));
+        System.out.println(IntegerList.get(2));
+        System.out.println(IntegerList.size());
+        System.out.println(IntegerList.isEmpty());
+        System.out.println(IntegerList.toArray());
+        IntegerList.clear();
     }
 }
